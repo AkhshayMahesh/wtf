b47f6abac96898d84e13eb743709fe323e716d4c
Dhsrthn <dhasarathan120904@gmail.com>
2025-03-09 18:31:22 +0530

    fix: signup overflow

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,11 @@ import { ImageSection } from "../components/Login/ImageSection";
 export default function LoginPage() {
 	return (
 		<main>
-			<div className="flex h-screen overflow-hidden font-['Poppins'] max-xl:flex-col-reverse">
+			<div className="flex h-screen font-['Poppins'] max-xl:flex-col-reverse">
 				<section className="max-xl:h-[75%] max-xl:w-full xl:w-[50%] 2xl:w-[40%]">
 					<LoginForm isLogin />
 				</section>
-				<section className="w-full max-xl:h-[25%]">
+				<section className="w-full max-xl:mb-2 max-xl:h-[25%]">
 					<ImageSection />
 				</section>
 			</div>
